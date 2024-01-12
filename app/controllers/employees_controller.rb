93a50c3f0b7bae3e7f52a25ca508278cdbc5a24a
Roshni Tiwari <rostiwari@bestpeers.in>
2024-01-12 10:49:36 +0530

    final commit

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,27 +1,28 @@
 class EmployeesController < ApplicationController
 
+  before_action :set_employee, only: [:edit, :update, :show, :destroy]
+
   def index
     @employees = Employee.all
   end
-def new 
-  @employee = Employee.new
-end
 
-def create
-  @employee = Employee.new(employee_params)
-    if @employee.save
-      redirect_to employees_path, notice: 'Employee has been created successfully'
-    else
-      render :new
-end
-end
+  def new 
+    @employee = Employee.new
+  end
+
+  def create
+    @employee = Employee.new(employee_params)
+      if @employee.save
+        redirect_to employees_path, notice: 'Employee has been created successfully'
+      else
+        render :new
+  end
+  end
 
 def edit
-  @employee = Employee.find(params[:id])
 end
 
 def update
-  @employee = Employee.find(params[:id])
   if @employee.update(employee_params)
     redirect_to employees_path, notice: 'Employee has been updated successfully'
     else
@@ -31,11 +32,9 @@ def update
 end
 
 def show
-  @employee = Employee.find(params[:id])
 end
 
 def destroy
-  @employee =  Employee.find(params[:id])
   if @employee.destroy
     redirect_to employees_path, notice: 'Employee has been deleted successfully'
   end
@@ -45,9 +44,14 @@ end
 
 
 private
-def employee_params
-  params.require(:employee).permit(:first_name, :middle_name,:last_name, :personal_email,:city, :state, :country, :pincode, :address_line_1,
-  :address_line_2)
-end
+  def employee_params
+    params.require(:employee).permit(:first_name, :middle_name,:last_name, :personal_email,:city, :state, :country, :pincode, :address_line_1,
+    :address_line_2)
+  end
 
+  def set_employee
+    @employee =  Employee.find(params[:id])
+  rescue ActiveRecord::RecordNotFound => error
+    redirect_to employees_path, notice: error
+  end
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,4 +1,7 @@
 class Employee < ApplicationRecord
+
+  has_many :documents
+
   validates :first_name, :last_name, presence: true
   validates :personal_email, presence: true, uniqueness: true
   validates :city, :state, :country, :pincode, :address_line_1, presence: true
@@ -10,5 +13,4 @@ class Employee < ApplicationRecord
   def full_address
     "#{city} #{state} #{country} #{pincode} #{address_line_1} "
   end
-
 end
